e801b9d78289e59a33df97cf19080659d6a7ed3f
Nevin Mital <mital.nevin@gmail.com>
2016-08-09 22:37:12 -0400

    Text size tweak for header on smaller screens, new background animation

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,12 +1,6 @@
-var $packery = $('.grid').packery({
-    itemSelector: '.grid-item',
-    columnWidth: 160,
-    isFitWidth: true,
-    stamp: '.stamp'
-});
-var resizeEvent;
-var openElement = null;
-
+var $bg = document.querySelector('#bgCanvas');
+var $bgElem = $('#bgCanvas');
+var $grid = $('.grid');
 var $header = $('.header');
 var $logoText = $('#logoText');
 var $logoM = $('#logoM');
@@ -15,6 +9,33 @@ var $headerButtons = $('#headerButtons');
 var $menuDropdown = $('#headerDropdown');
 var $menuTrigger = $('#dropdownTrigger');
 var $menuContent = $('#dropdownContent');
+
+var $ctx;
+var colorRows = [];
+var rowHeight = 120;
+var animFrames = 135;
+var updateIndex = 0;
+var numChange = 3;
+var animPaused = false;
+var unpauseAnim;
+
+var colors = [
+    {r:000,g:169,b:194}, //turquoise            'rgba(0, 169, 194, 1)'
+    {r:000,g:099,b:176}, //blue                 'rgba(0, 99, 176, 1)'
+    {r:097,g:192,b:212}, //light turquoise      'rgba(97, 192, 212, 1)'
+    {r:015,g:059,b:127}, //dark blue            'rgba(15, 59, 127, 1)'
+    {r:064,g:161,b:218}, //light blue           'rgba(64, 161, 218, 1)'
+    {r:012,g:041,b:073}  //default background   'rgba(12,41,73,1)'
+];
+
+var $packery = $grid.packery({
+    itemSelector: '.grid-item',
+    columnWidth: 160,
+    isFitWidth: true,
+    stamp: '.stamp'
+});
+var resizeEvent;
+var openElement = null;
 
 $packery.on('click', '.grid-item, .grid-item-expand', function(event){
     var item = $(event.currentTarget);
@@ -52,6 +73,20 @@ $packery.on('click', '.grid-item, .grid-item-expand', function(event){
 });
 
 $(window).resize(function () {
+    $bg.height = $grid.height();
+    $bg.width = $(window).innerWidth();
+
+    animPaused = true;
+    clearTimeout(unpauseAnim);
+    unpauseAnim = setTimeout(function() {
+        animPaused = false;
+        for(var r = 0; r < colorRows.length; r++){
+            colorRows[r].c1 = colors.length - 1;
+            colorRows[r].c2 = Math.floor(Math.random() * (colors.length - 1));
+            colorRows[r].frame = 0;
+        }
+    }, 300);
+
     clearTimeout(resizeEvent);
     resizeEvent = setTimeout(function () {
         $packery.packery();
@@ -99,3 +134,81 @@ $menuContent.css("display", "none");
 $menuTrigger.click(function(){
     $menuContent.slideToggle();
 });
+
+function colorName(index){
+    return 'rgba(' + colors[index].r + ',' + colors[index].g + ',' + colors[index].b + ',1)';
+}
+
+function lerpColor(c1, c2, frame){
+    var r = Math.round(colors[c1].r + (colors[c2].r - colors[c1].r) * frame / animFrames);
+    var g = Math.round(colors[c1].g + (colors[c2].g - colors[c1].g) * frame / animFrames);
+    var b = Math.round(colors[c1].b + (colors[c2].b - colors[c1].b) * frame / animFrames);
+    return 'rgba(' + r + ',' + g + ',' + b + ',1)';
+}
+
+$(function(){
+    anim_init();
+});
+
+function anim_init(){
+    $bg.height = $grid.height(); //120 * Math.ceil($(window).innerHeight() / 120);
+    $bg.width = $(window).innerWidth();
+
+    $ctx = $bg.getContext("2d");
+    $ctx.fillStyle = '#0c2949';
+    $ctx.fillRect(0, 0, $bg.width, $bg.height);
+
+    for(var y = 0; y < Math.ceil($bg.height / rowHeight); y++){
+        init_row(y);
+    }
+
+    window.requestAnimationFrame(anim_draw);
+}
+
+function init_row(i) {
+    colorRows[i] = {
+        c1: colors.length - 1,
+        c2: Math.floor(Math.random() * (colors.length - 1)),
+        frame: Math.floor(Math.random() * animFrames)
+    };
+}
+
+function reset_row(i){
+    colorRows[i].c1 = colorRows[i].c2;
+    while(colorRows[i].c2 == colorRows[i].c1){
+        colorRows[i].c2 = Math.floor(Math.random() * (colors.length - 1));
+    }
+    colorRows[i].frame = 0;
+}
+
+function anim_draw() {
+    if($(window).innerWidth() >= 705 && !animPaused) {
+        var changed = [];
+
+        for (var r = 0; r < numChange; r++) {
+            var y;
+            if(Math.ceil($bg.height / rowHeight) > numChange) {
+                do {
+                    y = Math.floor(Math.random() * Math.ceil($bg.height / rowHeight));
+                } while (changed.indexOf(y) != -1);
+                changed[updateIndex] = y;
+                updateIndex = (updateIndex + 1) % numChange;
+            } else {
+                y = r;
+            }
+
+            if (colorRows[y] == undefined) {
+                init_row(y);
+            }
+            if (colorRows[y].frame >= animFrames) {
+                reset_row(y);
+            }
+
+            $ctx.fillStyle = lerpColor(colorRows[y].c1, colorRows[y].c2, colorRows[y].frame);
+            $ctx.fillRect(0, y * rowHeight, $bg.width, rowHeight);
+            (colorRows[y].frame)++;
+        }
+    }
+
+    requestAnimationFrame(anim_draw);
+}
