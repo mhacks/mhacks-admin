5bd446ca51f44e246eea5b89d580b20fdd7511fe
Ankit Shah <ank.h.shah@gmail.com>
2016-08-20 01:57:22 -0700

    Being more explicit in __init__

--- MHacks/v1/serializers/__init__.py
+++ MHacks/v1/serializers/__init__.py
@@ -1,1 +1,4 @@
-from MHacks.v1.serializers.serializers import *
+from MHacks.v1.serializers.serializers import AnnouncementSerializer
+from MHacks.v1.serializers.serializers import AuthSerializer
+from MHacks.v1.serializers.serializers import EventSerializer
+from MHacks.v1.serializers.serializers import LocationSerializer
