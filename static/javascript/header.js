1b9cf182b081a10e2f37c5583466cd6b3a526085
Robert Allen <robby.allen24@gmail.com>
2017-02-03 23:12:52 -0500

    much smoother animation, synchronization between people and header

--- static/javascript/header.js
+++ static/javascript/header.js
@@ -1,5 +1,6 @@
 var $header = $('.header');
 var $headerPad = $('#header-pad');
+var $headerDropdown = $('#headerDropdown');
 var $logoText = $('#logo-text');
 var $logoScale = $('.logo-scale');
 var $eventInfo = $('#eventInfo');
@@ -9,69 +10,57 @@ var $menuTrigger = $('#dropdownTrigger');
 var $menuContent = $('#dropdownContent');
 
 var shouldScale = window.location.pathname == '/';
+
 $(document).ready(function(){
+    var scrollValue = Math.max($(window).scrollTop(), 0);
+    var screenHeight = $(window).innerHeight();
+    var multiplier = scrollValue / screenHeight;
     if (shouldScale){
         $header.css("height", "100%");
         $header.css("box-shadow", "none");
+        $header.css("z-index", 1);
+        if (multiplier < 1){
+            $logoScale.css("transform", "scale(" + (3 - multiplier * 2) + ")");
+            $eventInfo.css("opacity", 0);
+            $headerButtons.css("opacity", 0);
+            $headerDropdown.css("opacity", 0);
+        }
     } else {
         // not on home page
         $header.css("opacity", 0.95);
         $logoScale.css("transform", "scale(1)");
         $eventInfo.css("opacity", 1);
         $headerButtons.css("opacity", 1);
+        $headerDropdown.css("opacity", 1);
         $logoText.removeClass('logo-scale');
         $headerPad.css("padding-top", 60);
     }
 });
 
 $(window).scroll(function() {
-    var scrollValue = Math.max($(this).scrollTop(), 0);
-    
-    // additional animations done on the home page
-    if (shouldScale){    
-        if (scrollValue > 70) {
-            $logoScale.css("transform", "scale(1)");
-            $logoText.removeClass('logo-scale');
-            $header.css("opacity", 0.95);
-
-        } else {
-            $logoScale.css("transform", "scale(" + (100-scrollValue)/ 30 + ")");
-            $header.css("opacity", 1);
-            if (scrollValue < 60) $header.css("height", 100 - scrollValue + "vh");
-            if(scrollValue < 45) {    
-                if(scrollValue > 15) {
-                    $eventInfo.css("opacity", (scrollValue - 15.0) / 30.0);
-                    $headerButtons.css("opacity", (scrollValue - 15.0) / 30.0);
-                } else {
-                    $eventInfo.css("opacity", 0);
-                    $headerButtons.css("opacity", 0); 
-                }
-            } else {
-                $eventInfo.css("opacity", 1);
-                $headerButtons.css("opacity", 1);
-                $logoScale.css("transform", "scale(1)");
-                // shouldScale = false;
-
-            }
-        }
-    }
-    
-    // animations that are used on every page
-    if (scrollValue < 60) {
-        $header.removeClass("header-condensed");
-        $eventInfo.css("padding-left", 50 - 25 * scrollValue / 60);
-        $headerButtons.css("padding-right", 50 - 25 * scrollValue / 60);
-        $menuDropdown.css("padding-right", 50 - 25 * scrollValue / 60);
-        $menuContent.css("top", 120 - scrollValue);  
+    var scrollValue = Math.max($(window).scrollTop(), 0);
+    var screenHeight = $(window).innerHeight();
+    var screenWidth = $(window).innerWidth();
+    var multiplier = scrollValue / screenHeight;
+    if (multiplier < 1 && shouldScale){
+        $logoScale.css("transform", "scale(" + (3 - multiplier * 2) + ")");
+        $header.css("height", (1.1 - multiplier) * 100 + "vh");
+        $eventInfo.css("opacity", 0);
+        $headerButtons.css("opacity", 0);
+        $headerDropdown.css("opacity", 0);
     } else {
+        $header.css("opacity", 0.95);
+        $logoScale.css("transform", "scale(1)");
+        $eventInfo.css("opacity", 1);
+        $headerButtons.css("opacity", 1);
+        $headerDropdown.css("opacity", 1);
+        $logoText.removeClass('logo-scale');
+        $headerPad.css("padding-top", 60);
         $header.css("height", 60);
-        $header.addClass("header-condensed");
-        $eventInfo.css("padding-left", 25);
+        $header.css("z-index", 99999);
         $headerButtons.css("padding-right", 25);
         $menuDropdown.css("padding-right", 25);
         $menuContent.css("top", 60);
-        $eventInfo.css("opacity", 1);
-        $headerButtons.css("opacity", 1);
     }
 });
 
