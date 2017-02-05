e787faccb38736f34b51c1265b896bd8325ceb72
Nevin Mital <mital.nevin@gmail.com>
2017-02-05 03:02:55 -0500

    (hopefully) last header logo fix

--- static/javascript/header.js
+++ static/javascript/header.js
@@ -23,22 +23,22 @@ $(document).ready(function(){
         if (scrollValue < headerScroll){
             $header.css("opacity", 0.95 + 0.05 * (1 - scrollValue / headerScroll));
             $header.css("height", "calc(" + (1 - scrollValue / headerScroll) + "* (100vh - 100px) + 100px)");
-            $logoContainer.css("width", (1 - scrollValue / headerScroll) * 50 + 50 + "vw");
-            $logo.css("max-height", "calc((100% - 51px) * " + (scrollValue / headerScroll) + " + 51px)");
+            $logoContainer.css("width", (1 - scrollValue / headerScroll) * 60 + 40 + "vw");
+            //$logo.css("max-height", "calc((100% - 51px) * " + (1 - scrollValue / headerScroll) + " + 51px)");
         } else {
             $header.css("opacity", 0.95);
             //$headerPad.css("padding-top", 60);
             $header.css("height", 100);
-            $logoContainer.css("width", "50vw");
-            $logo.css("max-height", "51px");
+            $logoContainer.css("width", "40vw");
+            //$logo.css("max-height", "51px");
             $header.css("z-index", 99999);
         }
         $header.css("z-index", 1);
     } else {
         // not on home page
         $header.css("opacity", 0.95);
-        $logoContainer.css("width", "50vw");
-        $logo.css("max-height", "51px");
+        $logoContainer.css("width", "40vw");
+        //$logo.css("max-height", "51px");
         //$headerPad.css("padding-top", 60);
         $downScroll.hide();
     }
@@ -50,8 +50,8 @@ $(window).scroll(function() {
     if (scrollValue < headerScroll && shouldScale){
         $header.css("opacity", 0.95 + 0.05 * (1 - scrollValue / headerScroll));
         $header.css("height", "calc(" + (1 - scrollValue / headerScroll) + "* (100vh - 100px) + 100px)");
-        $logoContainer.css("width", (1 - scrollValue / headerScroll) * 50 + 50 + "vw");
-        $logo.css("max-height", "calc((100% - 51px) * " + (scrollValue / headerScroll) + " + 51px)");
+        $logoContainer.css("width", (1 - scrollValue / headerScroll) * 60 + 40 + "vw");
+        //$logo.css("max-height", "calc((100% - 51px) * " + (1 - scrollValue / headerScroll) + " + 51px)");
         if(scrollValue == 0){
             $downScroll.show();
         } else {
@@ -59,10 +59,9 @@ $(window).scroll(function() {
         }
     } else {
         $header.css("opacity", 0.95);
-        //$headerPad.css("padding-top", 60);
         $header.css("height", 100);
-        $logoContainer.css("width", "50vw");
-        $logo.css("max-height", "51px");
+        $logoContainer.css("width", "40vw");
+        //$logo.css("max-height", "51px");
         $header.css("z-index", 99999);
         $downScroll.hide();
     }
