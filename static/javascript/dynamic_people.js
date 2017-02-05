536ae7be9998faf46be9b210a571d29370c67b4d
Nevin Mital <mital.nevin@gmail.com>
2017-02-04 20:52:58 -0500

    Character Animation fixed

--- static/javascript/dynamic_people.js
+++ static/javascript/dynamic_people.js
@@ -1,21 +1,61 @@
-$person = $('.person');
-$people = $('.dynamic-people');
-$bubble = $('#bubble');
+var $person = $('.person');
+var $people = [$('#p1'), $('#p2'), $('#p3'), $('#p4'), $('#p5'), $('#p6'), $('#p7'), $('#p8')];
+var $bubble = $('#bubble');
 
-var headerScroll = 100;
+var animStart = 60;
+var headerScroll = 160;
+var animStops = [
+    {s: animStart, l: headerScroll - animStart},
+    {s: animStart + (headerScroll - animStart) / 4, l: (headerScroll - animStart) * 0.75},
+    {s: animStart + (headerScroll - animStart) / 2, l: (headerScroll - animStart) * 0.5},
+    {s: animStart + 3 * (headerScroll - animStart) / 4, l: (headerScroll - animStart) * 0.25}
+    ];
+var finalPos = [26, 19, 9, 5, -5, -18, -21, -32];
 
 $(window).scroll(function() {
     var scrollValue = Math.max($(this).scrollTop(), 0);
-    var screenHeight = $(this).innerHeight();
-    var screenWidth = $(this).innerWidth();
-    var multiplier = scrollValue / headerScroll;
-    if (multiplier < 1) {
-    	$person.css("margin-left", screenWidth * 0.055 * (1 - multiplier) + "vw");
-    	$person.css("margin-right", screenWidth * 0.055 * (1 - multiplier) + "vw");
+
+    if(scrollValue > animStart){
+        if(scrollValue > headerScroll){
+            $person.each(function(i, p){
+                $(p).css("transform", "translate(" + finalPos[$(p).data("place")] + "%,0)");
+            });
+            $bubble.css("opacity", 1);
+        } else {
+            $people[3].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[0].s) / animStops[0].l) + " * -50vw + " + finalPos[3] + "%),0)");
+            $people[4].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[0].s) / animStops[0].l) + " * 50vw + " + finalPos[4] + "%),0)");
+            if(scrollValue > animStops[1].s){
+                $people[2].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[1].s) / animStops[1].l) + " * -50vw + " + finalPos[2] + "%),0)");
+                $people[5].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[1].s) / animStops[1].l) + " * 50vw + " + finalPos[5] + "%),0)");
+                if(scrollValue > animStops[2].s){
+                    $people[1].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[2].s) / animStops[2].l) + " * -50vw + " + finalPos[1] + "%),0)");
+                    $people[6].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[2].s) / animStops[2].l) + " * 50vw + " + finalPos[6] + "%),0)");
+                    if(scrollValue > animStops[3].s){
+                        $people[0].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[3].s) / animStops[3].l) + " * -50vw + " + finalPos[0] + "%),0)");
+                        $people[7].css("transform", "translate(calc(" + (1 - (scrollValue - animStops[3].s) / animStops[3].l) + " * 50vw + " + finalPos[7] + "%),0)");
+                    } else {
+                        $people[0].css("transform", "translate(-50vw,0)");
+                        $people[7].css("transform", "translate(50vw,0)");
+                    }
+                } else {
+                    $people[1].css("transform", "translate(-50vw,0)");
+                    $people[6].css("transform", "translate(50vw,0)");
+                }
+            } else {
+                $people[2].css("transform", "translate(-50vw,0)");
+                $people[5].css("transform", "translate(50vw,0)");
+            }
+            $bubble.css("opacity", (scrollValue - animStops[0].s) / animStops[0].l);
+            /*$person.each(function(i, p){
+                var direction = (i < $person.length / 2) ? "-50vw" : "50vw";
+                $(p).css("transform", "translate(calc(" + (1 - (scrollValue - 60) / 100) + " * " + direction + " + " + finalPos[$(p).data("place")] + "%),0)");
+            });*/
+        }
     } else {
-    	$person.css("margin-left", "0");
-    	$person.css("margin-right", "0");
-    	
+        $person.each(function(i, p){
+            var direction = (i < $person.length / 2) ? "-50vw" : "50vw";
+            $(p).css("transform", "translate(" + direction + ",0)");
+        });
+        $bubble.css("opacity", 0);
     }
-    $bubble.css("opacity", multiplier - .2);
 });
--- static/javascript/header.js
+++ static/javascript/header.js
@@ -1,5 +1,5 @@
 var $header = $('.header');
-var $headerPad = $('#header-pad');
+//var $headerPad = $('#header-pad');
 var $headerDropdown = $('#headerDropdown');
 var $logoContainer = $('#logo');
 var $logo = $('#logo-text');
@@ -9,7 +9,7 @@ var $menuContent = $('#dropdownContent');
 var $downScroll = $('#scroll-down');
 
 var shouldScale = window.location.pathname == '/';
-var headerScroll = 100;
+var headerScroll = 160;
 
 $(document).ready(function(){
     if (shouldScale){
@@ -22,13 +22,13 @@ $(document).ready(function(){
 
         if (scrollValue < headerScroll){
             $header.css("opacity", 0.95 + 0.05 * (1 - scrollValue / headerScroll));
-            $header.css("height", "calc(" + (1 - scrollValue / headerScroll) + "* (100vh - 60px) + 60px)");
+            $header.css("height", "calc(" + (1 - scrollValue / headerScroll) + "* (100vh - 100px) + 100px)");
             $logoContainer.css("width", (1 - scrollValue / headerScroll) * 50 + 50 + "vw");
             $logo.css("max-height", "calc((100% - 51px) * " + (scrollValue / headerScroll) + " + 51px)");
         } else {
             $header.css("opacity", 0.95);
-            $headerPad.css("padding-top", 60);
-            $header.css("height", 60);
+            //$headerPad.css("padding-top", 60);
+            $header.css("height", 100);
             $logoContainer.css("width", "50vw");
             $logo.css("max-height", "51px");
             $header.css("z-index", 99999);
@@ -37,7 +37,7 @@ $(document).ready(function(){
     } else {
         // not on home page
         $header.css("opacity", 0.95);
-        $headerPad.css("padding-top", 60);
+        //$headerPad.css("padding-top", 60);
         $downScroll.hide();
     }
 });
@@ -47,7 +47,7 @@ $(window).scroll(function() {
 
     if (scrollValue < headerScroll && shouldScale){
         $header.css("opacity", 0.95 + 0.05 * (1 - scrollValue / headerScroll));
-        $header.css("height", "calc(" + (1 - scrollValue / headerScroll) + "* (100vh - 60px) + 60px)");
+        $header.css("height", "calc(" + (1 - scrollValue / headerScroll) + "* (100vh - 100px) + 100px)");
         $logoContainer.css("width", (1 - scrollValue / headerScroll) * 50 + 50 + "vw");
         $logo.css("max-height", "calc((100% - 51px) * " + (scrollValue / headerScroll) + " + 51px)");
         if(scrollValue == 0){
@@ -57,8 +57,8 @@ $(window).scroll(function() {
         }
     } else {
         $header.css("opacity", 0.95);
-        $headerPad.css("padding-top", 60);
-        $header.css("height", 60);
+        //$headerPad.css("padding-top", 60);
+        $header.css("height", 100);
         $logoContainer.css("width", "50vw");
         $logo.css("max-height", "51px");
         $header.css("z-index", 99999);
