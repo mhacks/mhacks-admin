b20b271756139a833a2c1a92f3188419be74a4a3
Connor Krupp <kruppcon@umich.edu>
2017-03-18 22:25:34 -0400

    Added map with overlay to live

--- static/javascript/maps.js
+++ static/javascript/maps.js
@@ -18,3 +18,14 @@ function selectFloor(l){
     $(".maps-link[data-level='" + l + "']").addClass("selected");
 }
 
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
