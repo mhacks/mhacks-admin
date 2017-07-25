d4ea66483641df90042bd2051617b1152ce97b34
Peter Kaplan <pkap@umich.edu>
2017-07-24 19:55:04 -0700

    Culling files not needed for Admin purposes

--- static/javascript/faq.js
+++
@@ -1,5 +0,0 @@
-$(".faq-item .question").click(function(event){
-    var q = $(event.target);
-    q.toggleClass("open");
-    $(".faq-item .answer[data-qid='" + q.data("qid") + "']").stop().slideToggle('medium');
-});
